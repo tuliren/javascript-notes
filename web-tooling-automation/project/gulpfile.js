956b869edc893de5b6cdc585961018a7238a9d81
Liren Tu <tuliren.git@outlook.com>
2019-05-27 19:29:01 -0700

    Add copy tasks and refactor

--- web-tooling-automation/project/gulpfile.js
+++ web-tooling-automation/project/gulpfile.js
@@ -9,7 +9,7 @@ const browserSync = require('browser-sync').create();
 const jasmineBrowser = require('gulp-jasmine-browser');
 
 gulp.task('styles', function() {
-  gulp
+  return gulp
     .src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(
@@ -17,24 +17,22 @@ gulp.task('styles', function() {
         browsers: ['last 2 versions']
       })
     )
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('lint', function() {
-  return (
-    gulp
-      .src(['js/**/*.js'])
-      // eslint() attaches the lint output to the eslint property
-      // of the file object so it can be used by other modules.
-      .pipe(eslint())
-      // eslint.format() outputs the lint results to the console.
-      // Alternatively use eslint.formatEach() (see Docs).
-      .pipe(eslint.format())
-      // To have the process exit with an error code (1) on
-      // lint error, return the stream and pipe to failOnError last.
-      .pipe(eslint.failOnError())
-  );
+  return  gulp
+    .src(['js/**/*.js'])
+    // eslint() attaches the lint output to the eslint property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failOnError last.
+    .pipe(eslint.failOnError());
 });
 
 gulp.task('test-console', function() {
@@ -51,13 +49,26 @@ gulp.task('test-browser', function() {
     .pipe(jasmineBrowser.server({ port: 3001 }));
 });
 
-// default default task
-gulp.task('default', function() {
+gulp.task('copy-html', function() {
+  return gulp
+    .src('./index.html')
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('copy-images', function() {
+  return gulp
+    .src('img/*')
+    .pipe(gulp.dest('dist/img'))
+});
+
+// default task
+gulp.task('default', gulp.series('copy-html', 'copy-images', 'styles', 'lint', function() {
   // watch for sass changes and run styles task
   watch('sass/**/*.scss', gulp.series('styles'));
   watch('js/**/*.js', gulp.series('lint'));
+  watch('/index.html', gulp.series('copy-html'));
 
   browserSync.init({
     server: "./"
   });
-});
+}));
