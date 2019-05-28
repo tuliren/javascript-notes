6b5899a089f27fd78e3642147858796770cb39ac
Liren Tu <tuliren.git@outlook.com>
2019-05-27 17:38:19 -0700

    Add browser-sync

--- web-tooling-automation/project/gulpfile.js
+++ web-tooling-automation/project/gulpfile.js
@@ -1,22 +1,28 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
-gulp.task("styles", function() {
+gulp.task('styles', function() {
   gulp
-    .src("sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(
       autoprefixer({
         browsers: ['last 2 versions']
       })
     )
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
 // default default task
-gulp.task("default", function() {
+gulp.task('default', function() {
   // watch for sass changes and run styles task
-  watch("sass/**/*.scss", gulp.series("styles"));
+  watch('sass/**/*.scss', gulp.series('styles'));
+
+  browserSync.init({
+    server: "./"
+  });
 });
