218abdf6214f13aa904e79d6879f82eee799bc7d
Liren Tu <tuliren.git@outlook.com>
2019-06-08 17:56:48 -0700

    Show first planet thumb

--- javascript-promises/exoplanet-explorer/app/scripts/app.js
+++ javascript-promises/exoplanet-explorer/app/scripts/app.js
@@ -1,11 +1,3 @@
-/*
-Instructions:
-(1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-  (a) Resolve on load and reject on error.
-(2) If the XHR resolves, use addSearchHeader to add the search header to the page.
-(3) If the XHR fails, console.log the error and pass 'unknown' to addSearchHeader
- */
-
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
@@ -17,14 +9,22 @@ Instructions:
   /**
    * Helper function to show the search query.
    * @param {String} response - The unparsed JSON response from get.
+   * @param {String} query - The search query.
    */
-  function addSearchHeader(response) {
-    try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
-    } catch (e) {
-      // it's 'unknown', so leave it alone
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+  }
+
+  /**
+   * Helper function to create a planet thumbnail.
+   * @param  {Object} data - The raw data describing the planet.
+   */
+  function createPlanetThumb(data) {
+    var pT = document.createElement('planet-thumb');
+    for (var d in data) {
+      pT[d] = data[d];
     }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+    home.appendChild(pT);
   }
 
   /**
@@ -32,7 +32,7 @@ Instructions:
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
-  function get(url) {
+  function getWithXhr(url) {
     return new Promise(function(resolve, reject) {
       var req = new XMLHttpRequest();
       req.open('GET', url);
@@ -50,30 +50,44 @@ Instructions:
     });
   }
 
-  function getWithFetch(url) {
+  /**
+   * Alternative method to {@code #get(url)}.
+   */
+  function get(url) {
     return fetch(url, {
       method: 'get'
     });
   }
 
+  /**
+   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * @param  {String} url - The JSON URL to fetch.
+   * @return {Promise}    - A promise that passes the parsed JSON response.
+   */
   function getJson(url) {
-    return getWithFetch(url).then(function(response) {
+    console.log(url);
+    return get(url).then(function(response) {
       return response.json();
     });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    // alternative: getJson('../data/earth-like-results.json')
-    get('../data/earth-like-results.json')
+    getJson('../data/earth-like-results.json')
       .then(function(response) {
-        // alternative: addSearchHeader(response.query);
-        addSearchHeader(response);
         console.log(response);
+        console.log(response.query);
+        addSearchHeader(response.query);
+        console.log(getJson(response.results[0]));
+        return getJson(response.results[0])
       })
-      .catch(function(error) {
+      .catch(function() {
+        throw Error('Search Request Error');
+      })
+      .then(createPlanetThumb)
+      .catch(function(e) {
         addSearchHeader('unknown');
-        console.log(error);
+        console.log(e);
       });
   });
 })(document);
