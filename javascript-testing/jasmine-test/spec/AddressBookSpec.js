c21ceeea59697b65359597ba7681a89833c5736b
Liren Tu <tuliren.git@outlook.com>
2019-08-20 00:01:20 -0700

    Dry test code

--- javascript-testing/jasmine-test/spec/AddressBookSpec.js
+++ javascript-testing/jasmine-test/spec/AddressBookSpec.js
@@ -1,16 +1,18 @@
 describe('Address Book', function() {
-  it('should be able to add a contact', function() {
-    const addressBook = new AddressBook(),
-          thisContact = new Contact();
+  let addressBook,
+      thisContact;
 
+  beforeEach(function() {
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
+  it('should be able to add a contact', function() {
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
   });
 
   it('should be able to delete a contact', function() {
-    const addressBook = new AddressBook(),
-      thisContact = new Contact();
-
     addressBook.addContact(thisContact);
     addressBook.deleteContact(0);
     expect(addressBook.getContact(0)).not.toBeDefined();
