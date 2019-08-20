798fd466b5cf07987c2135891bec3af827aec860
Liren Tu <tuliren.git@outlook.com>
2019-08-20 00:00:03 -0700

    Add a second spec

--- javascript-testing/jasmine-test/spec/AddressBookSpec.js
+++ javascript-testing/jasmine-test/spec/AddressBookSpec.js
@@ -6,4 +6,13 @@ describe('Address Book', function() {
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
   });
+
+  it('should be able to delete a contact', function() {
+    const addressBook = new AddressBook(),
+      thisContact = new Contact();
+
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
 });
--- javascript-testing/jasmine-test/src/AddressBook.js
+++ javascript-testing/jasmine-test/src/AddressBook.js
@@ -4,6 +4,9 @@ function AddressBook() {
 AddressBook.prototype.addContact = function(contact) {
   this.contacts.push(contact);
 };
+AddressBook.prototype.deleteContact = function(index) {
+  this.contacts.splice(index, 1);
+};
 AddressBook.prototype.getContact = function(index) {
   return this.contacts[index];
 };
